d6a8e95e416cd139b5d740a810b819967945413d
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-04 17:33:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ app.get('/article-one', function(req,res){
 //res.send('Article one will be served here');
 });
 app.get('/article-two', function(req,res){
-res.send('Article two will be served here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
+//res.send('Article two will be served here');
 });
 app.get('/article-three', function(req,res){
-res.send('Article three will be served here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+//res.send('Article three will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
