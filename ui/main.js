240ebc15026e0f3476369102f917f6f3dad2cd56
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-05 09:34:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,3 +37,24 @@ button.onclick= function()
     request.open('GET','http://kishorebaktha.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submitbtn=document.getElementById("submit");
+submitbtn.onclick=function()
+{
+  //make request to server and send the name
+  
+  //capture list of names render it as a list
+  var names=['name1','name2','name3'];
+  var list='';
+  for(var i =0;i<names.length;i++)
+  {
+      list+='<li>'+names[i]+'</li>';
+  }
+  var ul=document.getElementById("ul");
+  ul.innerHTML=list;
+};
+
+
