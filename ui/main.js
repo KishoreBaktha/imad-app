716150e2df7c8d88ad416577f9a02d2eef62f4f8
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-05 07:08:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById("main_text");
 element.innerHTML='new value';
 var image=document.getElementById("madi");
-image.onClick= function ()
+image.onclick= function ()
 {
     image.style.marginLeft='100px';
 }
