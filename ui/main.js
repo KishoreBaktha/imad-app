89c5899b6f6443e9e3a6d6551b747f290607e812
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-05 07:07:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("main_text");
 element.innerHTML='new value';
+var image=document.getElementById("madi");
+image.onClick= function ()
+{
+    image.style.marginLeft='100px';
+}
