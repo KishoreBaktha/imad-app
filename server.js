da04c9dbe7d0251521e4526af5dbfc34d1072575
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-05 08:03:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter',function(req,res) {
+   counter=counter+1;
+   res.send(counter.toString());
+});
 app.get('/:articleName', function(req,res){
     var articleName=req.params.articleName;
     res.send(template(articles[articleName]));
@@ -100,11 +105,6 @@ app.get('/:articleName', function(req,res){
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-var counter=0;
-app.get('/counter',function(req,res) {
-   counter=counter+1;
-   res.send(counter.toString());
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
