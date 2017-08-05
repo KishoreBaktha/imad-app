9af3592da2f36e217c4bc3a256738ed07c1fcf2e
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-05 08:47:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 //var element=document.getElementById("main_text");
 //element.innerHTML='new value';
 var image=document.getElementById("madi");
@@ -11,4 +11,19 @@ function moveRight()
 image.onclick= function ()
 {
     var interval=setInterval(moveRight,50);
+};
+*/
+//counter code
+var counter=0;
+var button=document.getElementById("counter");
+button.onclick= function()
+{
+    //make request to counter end point
+    
+    //capture response and store in variable
+    
+    //render the variable in the correct span
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
 };
