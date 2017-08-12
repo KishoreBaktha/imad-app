6cbdcd0aac0ec183f7522a2e7845174274ed68e1
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-12 09:04:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ var pool=new Pool(config);
 app.get('/test-db',function(req,res)
 {
  //make a select request
- pool.query("SELECT * FROM test",function(err,result)
+ pool.query("SELECT * FROM article",function(err,result)
  {
     if(err)
     {
