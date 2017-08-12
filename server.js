b837f229b839a540eddb56ff33ded432f7b03e3a
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-12 09:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.get('/counter',function(req,res) {
 // //res.send('Article one will be served here');
 // });
 app.get('/articles/:articleName', function(req,res){
-    pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName +"'",function(err,result)
+    pool.query("SELECT * FROM article WHERE title= '"+ req.params.articleName +"'",function(err,result)
     {
          if(err)
     {
