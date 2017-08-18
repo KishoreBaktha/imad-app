f79663d91ac97b1e1be10103fbd8d782acfc7969
KishoreBaktha <kishorebaktha@gmail.com>
2017-08-18 21:38:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 //DATABASE
 var Pool=require('pg').Pool;
 var config={
@@ -14,6 +15,8 @@ var config={
   };
 var app = express();
 app.use(morgan('combined'));
+//load josn object to req.body variable
+app.use(bodyParser.json());
 var articles={
           'article-one':{
     title:'Article one-Kishore Baktha',
@@ -118,6 +121,26 @@ app.get('/hash/:input', function(req,res)
 {
    var hashstring=hash(req.params.input,'this-is-some-random-string');
    res.send(hashstring);
+});
+app.post('/create-user', function(req,res)
+{
+  //username,password
+  //{"username":"kishore","password":"password"}
+  //JSON
+  var username=req.body.username;
+  var password=req.body.password;
+  var salt=crypto.RandomBytes(128).toString('hex');
+  var dbString=hash(password,salt);
+  pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+        res.send('User successfully created'+username);
+    }  
+  });
 });
 
 //database2
